0f96c5f07f0b377b18462e01dbc15bd8bcab289a
Marco Barão <marco.neves@connectcom.univesp.br>
2018-11-24 21:07:09 -0200

    Estilizando o SearchBox

--- src/components/SearchInput/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const SearchInput = props => {
-  const { handleChange } = props;
-  return <input type="text" onChange={handleChange} name="search" />;
-};
-
-export default SearchInput;
