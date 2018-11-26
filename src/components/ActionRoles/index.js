26655d055a398d26bc874e8f372b94e13389265c
Marco Barão <marco.neves@connectcom.univesp.br>
2018-11-26 16:37:20 -0200

    Criando funcionalidade para o SearchBox

--- src/components/ActionRoles/index.js
+++ src/components/ActionRoles/index.js
@@ -1,35 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './styles.scss';
 
 import searchIcon from '../../assets/img/search.svg';
 
-class ActionRoles extends Component {
-  state = {
-    search: ''
-  };
-
-  handleChange = e => {
-    this.setState({ search: e.target.value });
-  };
-
-  render() {
-    const { search } = this.state;
-    return (
-      <nav className="actions-roles">
-        <div className="search">
-          <input
-            type="text"
-            onChange={this.handleChange}
-            value={search}
-            name="search"
-            placeholder="Buscar"
-          />
-          <img src={searchIcon} alt="" />
-        </div>
-      </nav>
-    );
-  }
-}
+const ActionRoles = ({ search, handleSearch }) => (
+  <nav className="actions-roles">
+    <div className="search">
+      <input
+        type="text"
+        onChange={handleSearch}
+        value={search}
+        name="search"
+        placeholder="Buscar"
+      />
+      <img src={searchIcon} alt="" />
+    </div>
+  </nav>
+);
 
 export default ActionRoles;
