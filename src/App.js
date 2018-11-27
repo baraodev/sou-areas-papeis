e8a6a0d4f08e45228a7c78854ac6303a3877c32a
Marco Barão <marco.neves@connectcom.univesp.br>
2018-11-27 09:12:45 -0200

    Refatoração do componente App e criação de rotas e rotas de modals

--- src/App.js
+++ src/App.js
@@ -1,118 +1,16 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
-import ActionRoles from './components/ActionRoles';
-import TableRoles from './components/TableRoles';
-import { resolveComponents } from 'uri-js';
+import { ModalContainer } from 'react-router-modal';
 
-class App extends Component {
-  state = {
-    search: '',
-    roles: {
-      'Recursos humanos': [
-        {
-          id: 1,
-          name: 'Diretor de RH',
-          users: [
-            {
-              image: 'Imagenzinhas'
-            },
-            {
-              image: 'Imagenzinhas'
-            }
-          ],
-          active: true
-        },
-        {
-          id: 2,
-          name: 'Subdiretor de RH',
-          users: [
-            {
-              image: 'Imagenzinhas'
-            },
-            {
-              image: 'Imagenzinhas'
-            }
-          ],
-          active: true
-        }
-      ],
-      TI: [
-        {
-          id: 3,
-          name: 'Diretor de TI',
-          users: [
-            {
-              image: 'Imagenzinhas'
-            },
-            {
-              image: 'Imagenzinhas'
-            }
-          ],
-          active: true
-        },
-        {
-          id: 4,
-          name: 'Subdiretor de TI',
-          users: [
-            {
-              image: 'Imagenzinhas'
-            },
-            {
-              image: 'Imagenzinhas'
-            }
-          ],
-          active: true
-        }
-      ]
-    }
-  };
+import Routes from './Routes';
 
-  handleSearch = e => {
-    this.setState({ search: e.target.value });
-  };
-
-  filterRoles = roles => {
-    const { search } = this.state;
-
-    if (!search) return roles;
-
-    const patt = new RegExp(`${search}`, 'i');
-
-    const newRoles = {};
-    Object.keys(roles).forEach(key => {
-      newRoles[key] = roles[key].filter(role => patt.test(role.name));
-      if (!newRoles[key].length) delete newRoles[key];
-    });
-    return newRoles;
-  };
-
-  handleCheck = ({ target }) => {
-    const { roles } = this.state;
-    const newRoles = {};
-    Object.keys(roles).forEach(key => {
-      newRoles[key] = roles[key].map(role => {
-        if (role.id === Number(target.value)) {
-          return { ...role, active: !role.active };
-        }
-        return role;
-      });
-    });
-
-    this.setState({ roles: newRoles });
-  };
-
-  render() {
-    const { roles, search } = this.state;
-    return (
-      <Fragment>
-        <ActionRoles search={search} handleSearch={this.handleSearch} />
-        <TableRoles
-          roles={this.filterRoles(roles)}
-          handleCheck={this.handleCheck}
-        />
-      </Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Fragment>
+      <Routes />
+      <ModalContainer />
+    </Fragment>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'react-router-modal/css/react-router-modal.css';
 import './assets/styles/base.scss';
 
 import App from './App';
